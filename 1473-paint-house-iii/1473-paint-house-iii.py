908e9ab93d918096540815e431e4a30c9e0d531f
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-07-08 09:42:55 +0530

    Time: 2720 ms (34.37%), Space: 14.7 MB (89.15%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.py
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -2,7 +2,7 @@ class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
         dp, cur_dp = {(0,0):0}, defaultdict(lambda:float('inf'))
         for i, h in enumerate(houses):
-            #g means the number of groups upto now
+            
             for prev_color, g in dp:
                 for cur_color in (range(1, n + 1) if not h else [h]):
                     cur_g = g + (prev_color != cur_color)
