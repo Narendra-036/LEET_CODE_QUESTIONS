323f0f3579bab6b4284d85e55b465896c6e4c8b5
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-07-27 22:33:10 -0700

    Time: 40 ms (98.21%), Space: 14.4 MB (96.92%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            if s.count(c) != t.count(c): return False
+        return True
