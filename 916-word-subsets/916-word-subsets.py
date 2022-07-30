4c8bba9ecee14862fa5973c0545f95313f7f0663
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-07-30 02:22:18 -0700

    Time: 2013 ms (12.01%), Space: 18.6 MB (73.00%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,18 +1,12 @@
 class Solution(object):
-    def wordSubsets(self, A, B):
-        def count(word):
-            ans = [0] * 26
-            for letter in word:
-                ans[ord(letter) - ord('a')] += 1
-            return ans
-
-        bmax = [0] * 26
-        for b in B:
-            for i, c in enumerate(count(b)):
-                bmax[i] = max(bmax[i], c)
-
-        ans = []
-        for a in A:
-            if all(x >= y for x, y in zip(count(a), bmax)):
-                ans.append(a)
-        return ans
+    def wordSubsets(self, words1, words2):
+        result = []
+        tempDict = Counter()
+        for w in words2:
+            tempDict |= Counter(w)
+        print(tempDict)
+        
+        for w in words1:
+            if not tempDict - Counter(w):
+                result.append(w)
+        return result
