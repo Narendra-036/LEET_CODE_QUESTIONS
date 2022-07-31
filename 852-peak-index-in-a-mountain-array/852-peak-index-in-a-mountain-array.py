2119625cce88363b1cf54796095069fb434a0d26
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-07-30 19:46:17 -0700

    Time: 860 ms (22.97%), Space: 41.3 MB (12.18%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,5 +1,12 @@
 class Solution:
-    def peakIndexInMountainArray(self, A: List[int]) -> int:
-        for i in range(len(A)):
-            if A[i] > A[i+1]:
-                return i
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        def binarysearch(l,r):
+            mid = (l+r)//2
+            
+            if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1]:
+                return mid
+            elif arr[mid] < arr[mid-1]:
+                return binarysearch(l,mid)
+            elif arr[mid] < arr[mid+1]:
+                return binarysearch(mid,r)
+        return binarysearch(0,len(arr)-1)
