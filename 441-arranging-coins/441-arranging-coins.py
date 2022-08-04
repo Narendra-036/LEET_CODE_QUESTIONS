8dfbe886bdbacd65ff97833b398a435e7cdbce97
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-08-03 19:12:24 -0700

    Time: 793 ms (40.78%), Space: 13.8 MB (57.67%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        
+        i=1
+        count=0
+        while n>=0:
+            n-=i
+            i+=1
+        return i-2
+        
