5bb3b1e6d57fe329194858387230e8a3d8b84d6b
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-09-16 00:36:32 +0530

    Time: 3591 ms (8.65%), Space: 36.5 MB (5.12%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,11 +1,9 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
         if len(changed)%2!=0:
-            return [] # since changed is not a double-array then
-
+            return [] 
         d=Counter(changed)
-        """for c in changed:
-            d[c]=d.get(c,0)+1"""
+        
 
         ans=[]
         n=len(changed)
@@ -13,8 +11,7 @@ class Solution:
         for k in sorted(d.keys()):
             if d[k]!=0:
                 if k==0 and d[k]&1:
-                    # x&1 is a fast way to check if a number x is odd/even.
-                    # It is the same as x%2==1 but it's faster.
+                    
                     return []
                 ans += [k] * (d[k] // 2 if k == 0 else d[k])
                 d[2*k] -= d[k]
