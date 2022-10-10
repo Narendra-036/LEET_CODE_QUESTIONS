70688f42e62e1519aa21535f6bef16040529c089
Narendrakumarreddy-Kadiri <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-10-10 01:48:24 -0700

    Time: 59 ms (30.01%), Space: 13.9 MB (61.62%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,6 +1,9 @@
 class Solution:
     def breakPalindrome(self, s: str) -> str:
         
+        
+        
+        #checking for palindrome... 
         def palin(s):
             i=0
             j=len(s)-1
@@ -11,21 +14,35 @@ class Solution:
                 else:
                     return False
             return True
+        
+        #stats.....................
         if len(s)==1:
             return ""
+        
         else:
+            
+            
+            
             # st=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
             x=""
-            if len(s)==s.count("a"):
-                return s[:-1]+"b"
             for i in range(len(s)):
                 if s[i]!="a":
+                    
+                    #changing the current element with a
                     x=s[:i]+"a"+s[i+1:]
+                    
+                    
+                    
+                    #checking palindrome after changing
                     if palin(x):
                         continue
                     else:
                         return x
+                    
+            #if it is still palindrome then change the last letter....\U0001f44d
             if palin(x):
                 return s[:-1]+"b"
+            
+#HAPPY CODING...\U0001f973\U0001f973\U0001f973\U0001f973\U0001f973
             
             
