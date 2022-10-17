f0a0cbdb2cadcb2723f0c1b9a017e5f209e713ab
Narendra Kumar Reddy <101043393+Narendrakumarreddy-Kadiri@users.noreply.github.com>
2022-10-16 21:34:47 -0700

    Time: 60 ms (39.86%), Space: 13.9 MB (11.66%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        a=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        for i in a:
+            if i not in sentence:
+                return False
+        return True
