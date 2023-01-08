d31e227ea7d80d7fdb8c5892a1f0a3b3703663e5
Narendra Kumar Reddy <101043393+Narendra-036@users.noreply.github.com>
2023-01-08 06:36:12 +0530

    Time: 584 ms (96.51%), Space: 27.9 MB (52.91%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.py
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.py
@@ -1,17 +1,15 @@
 class Solution:
     def maximumTop(self, nums: List[int], k: int) -> int:
-        if len(nums)==1 and k%2==1:
-            return -1
-        ans=0
-        if k==0:
+        if k == 0:
             return nums[0]
-        while nums and k>1:
-            ans=max(ans,nums.pop(0))
-            k-=1
-        if k==1 and len(nums)>1:
-            return max(ans,nums[1])
-        elif k==1:
-            return ans
-        else:
-            return ans
-        print(k, ans, nums)
+        
+        if k % 2 == 1 and len(nums) == 1:
+            return -1
+        
+        if k > len(nums):
+            return max(nums)
+        
+        if k == len(nums):
+            return max(nums[:-1])
+            
+        return max(nums[:(k-1)] + [nums[k]])
