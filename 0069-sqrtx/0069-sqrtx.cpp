02e65ce34167a1e2b63cc2b93ffb23f9dec26702
Narendra Kumar Reddy <101043393+Narendra-036@users.noreply.github.com>
2023-03-16 11:02:44 +0530

    Time: 29 ms (11.54%), Space: 6 MB (8.15%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long int start =0;
+        // long int start =0;
         long int prev=0;
         // int mid=start+end/2;
         while(true)
         {
-            if (prev*prev<=x && start*start>x){
-                return prev;
-            }
-            prev=start;
-            start+=1;
+            if (prev*prev==x){return prev;}
+            else if(prev*prev>x){return prev-1;}
+            else{prev+=1;}
         }
     }
 };
