3f2e2c6132e07d523d08ad2fe12503c2c5946aca
Narendra Kumar Reddy <101043393+Narendra-036@users.noreply.github.com>
2023-03-29 22:53:32 +0530

    Time: 143 ms (87.87%), Space: 18.1 MB (45.06%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, c: List[List[int]]) -> List[List[int]]:
+        c=sorted(c,key=lambda x:x[0])
+        ans=[c[0]]
+        for i in c[1:]:
+            if i[0]<=ans[-1][1]:
+                ans[-1]=[min(ans[-1][0],i[0]),max(ans[-1][1],i[1])]
+            else:
+                ans.append(i)
+        return ans
+            
