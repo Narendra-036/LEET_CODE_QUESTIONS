26d7fdcc92c3905d33b56b560669cd0b6bd8985f
Narendra Kumar Reddy <101043393+Narendra-036@users.noreply.github.com>
2023-04-04 23:17:57 +0530

    Time: 136 ms (13.26%), Space: 16.4 MB (51.46%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -13,51 +13,31 @@ class Solution:
     def __init__(self):
         self.ans=0
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        a=0
+        
+        
+        
         def check(root,l):
-            # print("++++++")
-            # print(l.val,root.right.val)
             if l==None:
                 self.ans=1
                 return
-                
             if root.right and root.right.val==l.val:
                 check(root.right,l.next)
             if root.left and root.left.val==l.val:
                 check(root.left,l.next)
             
             
-                
-#             if l.next==None:
-#                 if (root.left and root.left.val==l.val) or (root.right and root.right.val==l.val):
-#                     print("==========")
-#                     a=1
-#                 return
-#             elif l.
-            
-#             elif l and root==None:
-#                 return 
-#             elif root.left and root.left.val==l.next.val:
-#                 print("&&&&&&&&&&&")
-#                 print(l.val)
-#                 check(root.left,l.next)
-#             elif root.right and root.right.val==l.next.val:
-#                 print("****")
-#                 print(l.val)
-#                 check(root.right,l.next)
-#             else:
-#                 return 
             
         def dfs(root):
-            
             if root==None:
                 return
             if root.val==head.val:
-                
                 check(root,head.next)
-        
             dfs(root.left)
             dfs(root.right)
+            
+            
+            
+            
         dfs(root)
         if self.ans==1:
             return True
